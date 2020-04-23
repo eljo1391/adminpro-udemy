8b2978d376b7aacb8e95686d7640eb09a9575315
Elias <eljo1391@gmail.com>
2020-04-23 19:57:59 -0400

    Modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//rutas
+// rutas
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
@@ -11,7 +11,7 @@ import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
-  { path: '', 
+ /* { path: '',
     component: PagesComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent},
@@ -19,10 +19,9 @@ const routes: Routes = [
       { path: 'graficas1', component: Graficas1Component},
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
-  },
+  },*/
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: '**', component: NopagefoundComponent}
 ];
 
 @NgModule({
